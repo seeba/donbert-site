862693a65bdaa2001ebe90afb400b7b274437e4d
Sebastian Pluta <poczta@sebastianpluta.pl>
2024-01-19 23:08:27 +0100

    add category manager

---
+++ src/Product/Domain/Service/CategoryManager.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Product\Domain\Service;
+
+use App\Product\Domain\Repository\CategoryRepositoryInterface;
+
+class CategoryManager implements CategoryManagerInterface
+{
+   public function __construct(
+        private CategoryRepositoryInterface $repository
+    )
+   {
+   }
+   
+   public function getParentCategoryChoices()
+   {
+    $categories = $this->repository->findAll();
+    $choices = [];
+
+    foreach ($categories as $category) {
+        $choices[$category->getId()] = $category->getName();
+    }
+
+    return $choices;
+   }
+}
