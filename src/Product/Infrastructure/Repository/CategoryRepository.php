de9a539071fbd01088d13bc79fc1df82c300c722
Sebastian Pluta <poczta@sebastianpluta.pl>
2024-01-22 23:38:59 +0100

    fix repository

--- src/Product/Infrastructure/Repository/CategoryRepository.php
+++ src/Product/Infrastructure/Repository/CategoryRepository.php
@@ -7,16 +7,16 @@ namespace App\Product\Infrastructure\Repository;
 use App\Product\Infrastructure\Entity\Category;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 final class CategoryRepository extends ServiceEntityRepository
 {
-    public function __construct(
-        private EntityManagerInterface $entityManager
-    )
+    public function __construct(ManagerRegistry $registry)
     {
+        parent::__construct($registry, Category::class);
     }
     public function save(Category $category): void
     {
-        $this->entityManager->persist($category);
+        $this->getEntityManager()->persist($category);
     }
 }
