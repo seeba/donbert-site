c7795c1a23458c2f91b017fef5d487803ae83776
Sebastian Pluta <poczta@sebastianpluta.pl>
2024-01-22 23:39:37 +0100

    create handler class

--- src/Product/Application/Command/Sync/CreateCategoryCommandHandler.php
+++ src/Product/Application/Command/Sync/CreateCategoryCommandHandler.php
@@ -4,3 +4,14 @@ declare(strict_types=1);
 
 namespace App\Product\Application\Command\Sync;
 
+use App\Shared\Application\Command\Sync\CommandHandlerInterface;
+
+class CreateCategoryCommandHandler implements CommandHandlerInterface
+{
+    public function __invoke(CreateCategoryCommand $command)
+    {
+        
+    }
+
+}
+
