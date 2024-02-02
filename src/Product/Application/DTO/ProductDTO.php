383a43821f5c33595d1ff07434571f7c726ec9e9
Sebastian Pluta <poczta@sebastianpluta.pl>
2024-02-02 23:55:27 +0100

    create

---
+++ src/Product/Application/DTO/ProductDTO.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Product\Application\DTO;
+
+class ProductDTO 
+{
+    public function __construct(
+        public ?string $name = null,
+        public ?array $categoriesIds = null
+    )
+    {
+    }   
+}
