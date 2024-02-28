98a09007398bc58d6ccfc18e9c6b2b546e720132
Sebastian Pluta <poczta@sebastianpluta.pl>
2024-02-29 00:30:42 +0100

    add fields

--- src/Product/Infrastructure/UI/Web/Form/ProductType.php
+++ src/Product/Infrastructure/UI/Web/Form/ProductType.php
@@ -5,16 +5,33 @@ declare(strict_types=1);
 namespace App\Product\Infrastructure\UI\Web\Form;
 
 use App\Product\Application\DTO\ProductDTO;
+use App\Product\Domain\Service\CategoryManagerInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
 {
+    public function __construct(
+        private CategoryManagerInterface $categoryManager
+    )
+    {
+    }
+    
+    
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('name')
+            ->add('categoriesIds', ChoiceType::class, [
+                'choices' => $this->categoryManager->getParentCategoryChoices(),
+                'required' => true,
+                'multiple' => true,
+                'expanded' => true
+            ])
+            ->add('save', SubmitType::class)
         ;
 
     }
