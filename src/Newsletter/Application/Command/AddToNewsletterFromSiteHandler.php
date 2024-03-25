2c07c3007bf3c69024aa4d7b825d4256c0b002d3
Sebastian Pluta <poczta@sebastianpluta.pl>
2024-03-25 23:45:01 +0100

    replace validation

--- src/Newsletter/Application/Command/AddToNewsletterFromSiteHandler.php
+++ src/Newsletter/Application/Command/AddToNewsletterFromSiteHandler.php
@@ -8,14 +8,16 @@ use App\Newsletter\Application\Command\AddToNewsletterFromSiteCommand;
 use App\Newsletter\Domain\Model\NewsletterConsent;
 use App\Newsletter\Domain\Model\NewsletterConsentId;
 use App\Newsletter\Domain\Repository\NewsletterConsentRepositoryInterface;
-
+use Exception;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[AsMessageHandler]
 class AddToNewsletterFromSiteHandler
 {
     public function __construct(
-        private NewsletterConsentRepositoryInterface $newsletterConsentRepository
+        private NewsletterConsentRepositoryInterface $newsletterConsentRepository,
+        private ValidatorInterface $validator
     )
     {
         
@@ -28,6 +30,19 @@ class AddToNewsletterFromSiteHandler
             $command->email,
             $command->createdAt
         );
+
+        $message = '';
+            $errors = $this->validator->validate($newsletterConsent);
+        //    dd($errors);
+            if ($errors->count() > 0) {
+                foreach ($errors as $error) {
+                    $message .= $error->getMessage();
+                }
+
+            throw new Exception($message);
+            }
+
+
 
         $this->newsletterConsentRepository->save($newsletterConsent);
     }
