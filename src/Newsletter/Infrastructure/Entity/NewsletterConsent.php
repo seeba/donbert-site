bb5d558ad7577fe5aaa05d1148b230c07f54758a
Sebastian Pluta <poczta@sebastianpluta.pl>
2024-03-25 23:45:33 +0100

    configure entity

--- src/Newsletter/Infrastructure/Entity/NewsletterConsent.php
+++ src/Newsletter/Infrastructure/Entity/NewsletterConsent.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace App\Newsletter\Infrastructure\Entity;
 
+use App\Newsletter\Infrastructure\Repository\NewsletterConsentRepository;
+use App\Shared\Application\Service\CanBeValidatedInterface;
+use DateTime;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
-#[ORM\Entity()]
+#[UniqueEntity(fields:["email"], message:'Ten adres email został już użyty')]
+#[ORM\Entity(repositoryClass:NewsletterConsentRepository::class)]
 #[ORM\Table(name:"newsletter_consents")]
-class NewsletterConsent 
+class NewsletterConsent implements CanBeValidatedInterface
 {
 
     #[ORM\Id]
@@ -17,15 +23,36 @@ class NewsletterConsent
     private string $id;
 
     #[ORM\Column(type:Types::STRING, length:255)]
+    #[Assert\Email]
     private string $email;
+
+    #[ORM\Column(type:Types::DATETIME_MUTABLE)]
+    private DateTime $createdAt;
 
     public function __construct(
         string $id,
-        string $email
+        string $email, 
+        Datetime $createdAt
     )
     {
         $this->id = $id;
         $this->email = $email;
+        $this->createdAt = $createdAt;
+    }
+
+    public function getId(): string 
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getCreatedAt(): DateTime 
+    {
+        return $this->createdAt;
     }
 
 }
